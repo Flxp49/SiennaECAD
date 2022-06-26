2705d4db5f6a29c2a3d2d2de32c6478b76a23431
flxp49 <emxdahmad@gmail.com>
2022-06-26 13:54:05 +0530

    changed algo

--- compare.py
+++ compare.py
@@ -1,48 +1,23 @@
 import os
-import csv
 import pandas as pd
 files = []
-data = []
-match = []
-mismatch = []
 
-def process(file):
-    fileData = []
-    with open(file, mode ='r') as f:
-        csvFile = csv.reader(f)
-        next(f)
-        for line in csvFile:
-            fileData.append(line)
-    f.close()
-    data.append(fileData)
 
 def compare():
-    for cmp in data[0]: 
-        m = 0
-        for j in data[1]:
-            if cmp[0] == j[0] and cmp[1] == j[1]:
-                match.append(cmp)
-                m = 1
-                break
-        if (m != 1):
-            mismatch.append(cmp)
-    for cmp in data[1]: 
-        m = 0
-        for j in data[0]:
-            if cmp[0] == j[0] and cmp[1] == j[1]:
-                match.append(cmp)
-                m = 1
-                break
-        if (m != 1):
-            mismatch.append(cmp)
-    print(len(match))
-    print(len(mismatch))
+    df1 = pd.read_csv(files[0])
+    df2 = pd.read_csv(files[1])
+    df1 = pd.concat([df1, df2]).drop_duplicates(keep="first")
+    print(df1)
+    mdf = df1[df1.duplicated("Component Name", keep=False)].drop_duplicates(keep="first")
+    print(mdf)
+    misdf = pd.concat([df1, mdf]).drop_duplicates(keep=False)
+    print(misdf)
     current_directory = os.getcwd()
     final_directory = os.path.join(current_directory, r'Compare')
     if not os.path.exists(final_directory):
         os.makedirs(final_directory)
-    pd.DataFrame(match, columns=['Net Name', 'Component Name']).drop_duplicates(keep=False).to_csv("Compare/Match.csv")
-    pd.DataFrame(mismatch, columns=['Net Name', 'Component Name']).drop_duplicates(keep=False).to_csv("Compare/Mismatch.csv")
+    mdf.to_csv("Compare/Match.csv")
+    misdf.to_csv("Compare/Mismatch.csv")
 
 
 def main():     
@@ -51,8 +26,6 @@ def main():
     n=2
     for i in range(n):
         files.append(input("Enter filename" + str(i+1)+": "))
-    for file in files:
-        process(file)
     compare()
     # else:
         # print("Please enter >1")
